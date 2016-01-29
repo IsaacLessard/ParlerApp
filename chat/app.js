155ce9e68498899094d5a4b770b60b14c7d5263a
Addison <addisonbennett@gmail.com>
2016-01-29 10:19:06 -0700

    added app.js

---
+++ chat/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log("someone entered the chat room!");
+});
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+server.listen(3000, function(){
+  console.log('listening on localhost:3000');
+});
